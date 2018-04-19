1f06309574321741c4007698bd89b1135b90c205
cwang101 <625250454@qq.com>
2018-04-19 18:56:58 +0800

    add annotaion

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,8 +19,10 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
     Employee findFirstByNameContainingAndSalaryGreaterThan(String name,int salary);
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
-    @Query( value="select * from  employee where companyId= ?1 order by salary desc limit 1" , nativeQuery = true)
-    Employee findHighestSalary(int companyId);
+//    @Query( value="select * from  employee where companyId= ?1 order by salary desc limit 1" , nativeQuery = true)
+//    Employee findHighestSalary(int companyId);
+    @Query(value = "SELECT * FROM employee t WHERE t.companyId =:companyId ORDER BY salary DESC limit 1", nativeQuery = true)
+    Employee findHighestSalary(@Param("companyId") Integer companyId);
     //4.实现对Employee的分页查询，每页两个数据
     Page<Employee> findAll(Pageable pageable);
     //5.查找**的所在的公司的公司名称
